43eb7a3240696669abdf8e9569ee1ff0c2132171
Jason <jason974448@gmail.com>
2025-09-10 11:39:59 -0300

    Got a camera working

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,14 +1,36 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
-public class CameraMovement
+public class CameraMovement : MonoBehaviour
 {
-    public float RotationSpeed = 1000.0f;
+    public Transform target;
+
+    public float RotationSpeed = 500.0f;
     void Update()
     {
-
+        if (Input.GetMouseButton(0))
+        {
+            Cursor.visible = false;
+            CamRotation();
+        }
+        
+        else
+        {
+            Cursor.visible = true;
+        }
     }
 
+    private void CamRotation()
+    {
 
+        if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
+        {
+            float verticalInput = Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime;
+            float horizontalInput = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
+            transform.RotateAround(target.position, Vector3.up, horizontalInput);
+            transform.RotateAround(target.position, transform.right, -verticalInput);
+        }
+    }
 
 }
